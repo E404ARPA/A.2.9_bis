a7bf74c9ada37771366167a9675b571dff78597b
E404ARPA <dam11.2020.jesuitas@gmail.com>
2020-11-29 00:25:20 +0100

    fix

--- app/src/main/java/com/arpa404/a29/MainActivity.java
+++ app/src/main/java/com/arpa404/a29/MainActivity.java
@@ -2,6 +2,7 @@ package com.arpa404.a29;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -14,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),getString(R.string.date_picker));
     }
 
     public void onClickShowAlert(View view) {
@@ -41,5 +47,17 @@ public class MainActivity extends AppCompatActivity {
                 });
         // Create and show the AlertDialog.
         myAlertBuilder.show();
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month + 1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string +
+                "/" + day_string +
+                "/" + year_string);
+
+        Toast.makeText(this, getString(R.string.date) + dateMessage,
+                Toast.LENGTH_SHORT).show();
     }
 }
